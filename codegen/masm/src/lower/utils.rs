5c13e5dbf368729c27fd81b58c20150d90c3a721
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-05-26 18:39:56 -0300

    WIP: Change World in favor of SymbolTableHolder
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- codegen/masm/src/lower/utils.rs
+++ codegen/masm/src/lower/utils.rs
@@ -611,10 +611,13 @@ mod tests {
 
         let mut builder = OpBuilder::new(context.clone());
 
-        let world_ref = builder.create::<builtin::World, ()>(Default::default())()
-            .expect("Error unrelated to test: Failed to build world.");
-        let mut world_builder = WorldBuilder::new(world_ref);
-        let world = &mut world_builder.world.borrow_mut().as_symbol_table_ref();
+        let symbol_table_holder =
+            builder.create::<test::SymbolTableHolder, ()>(Default::default())()
+                .expect("Error unrelated to test: Failed to build symbol table holder.");
+        let mut prim_symbol_table_builder =
+            test::PrimSymbolTableHolderBuilder::new(symbol_table_holder);
+        let symbol_table_ref =
+            &mut prim_symbol_table_builder.sym_table_holder.borrow_mut().as_symbol_table_ref();
 
         let function_ref = builder.create_function(
             Ident::with_empty_span("test".into()),
@@ -622,7 +625,7 @@ mod tests {
                 [AbiParam::new(Type::U32), AbiParam::new(Type::U32)],
                 [AbiParam::new(Type::U32)],
             ),
-            world,
+            symbol_table_ref,
         )?;
 
         let (a, b) = {
@@ -846,10 +849,13 @@ mod tests {
     ) -> Result<(FunctionRef, masm::Block), Report> {
         let mut builder = OpBuilder::new(context.clone());
 
-        let world_ref = builder.create::<builtin::World, ()>(Default::default())()
-            .expect("Error unrelated to test: Failed to build world.");
-        let mut world_builder = WorldBuilder::new(world_ref);
-        let world = &mut world_builder.world.borrow_mut().as_symbol_table_ref();
+        let symbol_table_holder =
+            builder.create::<test::SymbolTableHolder, ()>(Default::default())()
+                .expect("Error unrelated to test: Failed to build symbol table holder.");
+        let mut prim_symbol_table_builder =
+            test::PrimSymbolTableHolderBuilder::new(symbol_table_holder);
+        let symbol_table_ref =
+            &mut prim_symbol_table_builder.sym_table_holder.borrow_mut().as_symbol_table_ref();
 
         let function_ref = builder.create_function(
             Ident::with_empty_span("test".into()),
@@ -857,7 +863,7 @@ mod tests {
                 [AbiParam::new(Type::U32), AbiParam::new(Type::U32)],
                 [AbiParam::new(Type::U32)],
             ),
-            world,
+            symbol_table_ref,
         )?;
 
         let (a, b) = {
